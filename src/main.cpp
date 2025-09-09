9e65388e2947599fead5b4e11c2660c2699826ab
AlexandreGendronCM <2175441@cmontmorency.qc.ca>
2025-09-09 17:15:27 -0400

    exe1

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,33 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define MA_BROCHE_BOUTON 39
+#define MA_BROCHE_ANGLE 32
+
+
+#include <MicroOscSlip.h>
+// Le nombre 128 entre les < > ci-dessous est le nombre maximal d'octets réservés pour les messages entrants.
+// Les messages sortants sont écrits directement sur la sortie et ne nécessitent pas de réservation d'octets supplémentaires.
+MicroOscSlip<128> monOsc(&Serial);
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  
+Serial.begin(115200);
+
+pinMode( MA_BROCHE_BOUTON , INPUT );
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int maLectureBouton = digitalRead( MA_BROCHE_BOUTON );
+  int lectureAnalogique = analogRead(MA_BROCHE_ANGLE);
+  // Serial.println(maLectureBouton);
+  
+  monOsc.sendInt("/bouton",maLectureBouton);
+  monOsc.sendInt("/angle",lectureAnalogique);
+  
+  
+  delay(100);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
